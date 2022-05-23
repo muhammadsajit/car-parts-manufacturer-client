f27e6ffabdca75428f88285927a2cc56cb69ac60
muhammadsajit <sajit.du.niter@gmail.com>
2022-05-24 01:02:52 +0600

    footer added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../../Footer/Footer';
 import Banner from './Banner';
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <h2 className='text-center text-blue-500 text-4xl font-bold'>Car Parts Manufacturer</h2>
             <Banner></Banner>
+            <Footer></Footer>
         </div>
     );
 };
