4ed8ccc24264399bc9d415dd4472ee54594f6de5
muhammadsajit <sajit.du.niter@gmail.com>
2022-05-24 10:25:06 +0600

    social login added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import auth from '../../firebase.init';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 const Login = () => {
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+    if(gUser){
+        console.log(gUser)
+    }
+    
     return (
-        <div>
-            <h1>This is login</h1>
-        </div>
+        <div className='flex h-screen justify-center items-center'>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-center text-3xl font-bold">Login</h2>
+
+                    <div className="divider">OR</div>
+                    <button onClick={() => signInWithGoogle()} className="btn btn-outline">Continue With Google</button>
+
+                </div>
+                </div>
+            </div>
+
+        
     );
 };
 
